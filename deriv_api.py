e3adb663329afbb174bbb40c2cfbcf248d94d18b
sylennt <160680535+sylennt@users.noreply.github.com>
2026-01-17 22:52:09 +0100

    Update deriv_api.py

--- deriv_api.py
+++ deriv_api.py
@@ -1,20 +1,20 @@
-import websocket
-import json
+import websocket, json
 
-def get_candles(symbol, timeframe, count=300):
-    ws = websocket.create_connection("wss://ws.derivws.com/websockets/v3?app_id=1089")
+def get_candles(symbol, granularity, count=300):
+    ws = websocket.create_connection(
+        f"wss://ws.derivws.com/websockets/v3?app_id=1089"
+    )
 
     payload = {
         "ticks_history": symbol,
         "style": "candles",
-        "granularity": timeframe,
+        "granularity": granularity,
         "count": count
     }
 
     ws.send(json.dumps(payload))
-    response = json.loads(ws.recv())
+    data = json.loads(ws.recv())
     ws.close()
 
-    candles = response["candles"]
-    return candles
+    return data["candles"]
 
