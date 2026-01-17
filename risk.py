ebbb60dbfd73c3cff3e9032b01bd6b5bd23f8348
sylennt <160680535+sylennt@users.noreply.github.com>
2026-01-17 02:02:36 +0100

    Update risk.py

--- risk.py
+++ risk.py
@@ -1,1 +1,12 @@
+def trade_levels(entry, direction):
+    sl_points = 20
+
+    if direction == "BUY":
+        sl = entry - sl_points
+        tp = entry + sl_points * 2
+    else:
+        sl = entry + sl_points
+        tp = entry - sl_points * 2
+
+    return sl, tp
 
