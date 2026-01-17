a75e77323d96ef7217c6e0c125b5c9efb42f1284
sylennt <160680535+sylennt@users.noreply.github.com>
2026-01-17 02:01:57 +0100

    Update structure.py

--- structure.py
+++ structure.py
@@ -1,1 +1,9 @@
+def structure_levels(candles):
+    highs = [c["high"] for c in candles[-50:]]
+    lows = [c["low"] for c in candles[-50:]]
+
+    resistance = max(highs)
+    support = min(lows)
+
+    return support, resistance
 
