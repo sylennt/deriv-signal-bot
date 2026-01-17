7774076a9bee0dcabfc826e610dcef50649c59ec
sylennt <160680535+sylennt@users.noreply.github.com>
2026-01-17 03:11:24 +0100

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,1 +1,20 @@
+import requests
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+def send_signal(trade):
+    message = f"""
+📊 {trade['symbol']}
+📌 {trade['direction'].upper()}
+Entry: {trade['entry']}
+SL: {trade['stop']}
+TP: {trade['target']}
+RR: {trade['rr']}
+Lot: {trade['lot']}
+"""
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    requests.post(url, data={
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message
+    })
 
