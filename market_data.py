25f886d73d92ba4018f61da170292801a4b92253
sylennt <160680535+sylennt@users.noreply.github.com>
2026-01-17 22:52:50 +0100

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -1,12 +1,6 @@
 import pandas as pd
 
-def candles_to_df(candles):
+def to_df(candles):
     df = pd.DataFrame(candles)
     df["time"] = pd.to_datetime(df["epoch"], unit="s")
-    df = df.rename(columns={
-        "open": "open",
-        "high": "high",
-        "low": "low",
-        "close": "close"
-    })
     return df[["time", "open", "high", "low", "close"]]
